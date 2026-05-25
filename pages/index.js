3d3ef46603b3c0eff5623cd3e1c26b8fcf8d40df
Bolu <badegbemile@gmail.com>
2026-05-25 19:31:41 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ import Section from '../components/section'
 import NextLink from 'next/link'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import { BioSection, BioYear } from '../components/bio'
-import { IoLogoTwitter, IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5'
+import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5'
 
 const Page = () => {
   return (
